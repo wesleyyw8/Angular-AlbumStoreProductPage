4e1e167b2f15f7687807dac15a90a840a2fbbf4d
wesley <wesleyyw8@gmail.com>
2019-05-19 15:56:10 +0100

    a

---
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Album } from 'app/album';
+import { ProductService } from 'app/product.service';
+
+@Component({
+  selector: 'app-product-tracklisting',
+  templateUrl: './product-tracklisting.component.html',
+  styleUrls: ['./product-tracklisting.component.css']
+})
+export class ProductTracklistingComponent implements OnInit {
+
+  albumInfo: Album;
+  constructor(private _productService: ProductService) { }
+
+  ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response =>
+      this.albumInfo = response);
+  }
+}
